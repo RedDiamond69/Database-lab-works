f9781cd3e3ac73d3726374a371d6c8776113cdf2
Alexey Kosyuk <reddiamond821@gmail.com>
2019-11-18 02:05:42 +0300

    Completed task 6. Updated report.

--- Task 6/V8.sql
+++ Task 6/V8.sql
@@ -1,6 +1,38 @@
 USE AdventureWorks2012;
 GO
 
+DROP PROCEDURE [Production].[OrdersSummaryByMonths];
+GO
 
+CREATE PROCEDURE [Production].[OrdersSummaryByMonths]
+    @chosenMonths NVARCHAR(200)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	DECLARE @sqlExpression NVARCHAR(512);
+	SET @sqlExpression = 
+		'SELECT [Year],' + @chosenMonths + '
+		FROM
+		(
+			SELECT
+				YEAR([W].[DueDate]) AS [Year],
+				DATENAME(MONTH, [W].[DueDate]) AS [Month],
+				[W].[OrderQty] AS [OrdersSummary]
+			FROM
+				[Production].[WorkOrder] AS [W]
+		) AS [WO]
+		PIVOT
+		(
+			SUM(OrdersSummary)
+			FOR Month
+			IN (' + @chosenMonths + ')
+		) AS [Summary]';
+
+	EXEC sp_executesql @sqlExpression;
+END
+GO
+
+EXECUTE [Production].[OrdersSummaryByMonths] '[January],[February],[March],[April],[May],[June]';
 
 GO
