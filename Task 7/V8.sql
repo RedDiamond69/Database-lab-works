c4678a4f0a94c0ec127a8897c815dcd607e9d705
Alexey Kosyuk <reddiamond821@gmail.com>
2019-12-07 17:55:29 +0300

    Completed task 7. Updated report. Added generated XML.

--- Task 7/V8.sql
+++ Task 7/V8.sql
@@ -1,6 +1,33 @@
 USE AdventureWorks2012;
 GO
 
+CREATE PROCEDURE XmlToTable
+	@xml XML
+AS
+BEGIN
+	SELECT
+		Address.value('@ID', 'INT') AS [AddressID],
+		Address.value('(City)[1]', 'NVARCHAR(30)') AS [City],
+		Address.value('(Province/@ID)[1]', 'INT') AS [StateProvinceID],
+		Address.value('(Province/Region)[1]', 'NVARCHAR(3)') AS [CountryRegionCode]
+	FROM @xml.nodes('Addresses/Address') AS X(Address);
+END;
 
+GO
+
+DECLARE @xml XML = 
+	(SELECT
+		[ADR].[AddressID] AS '@ID',
+		[ADR].[City],
+		[SP].[StateProvinceID] AS 'Province/@ID',
+		[SP].[CountryRegionCode] AS 'Province/Region'
+	FROM [Person].[Address] AS [ADR]
+		INNER JOIN [Person].[StateProvince] AS [SP]
+		ON [ADR].[StateProvinceID] = [SP].[StateProvinceID]
+	FOR XML PATH('Address'), ROOT('Addresses'));
+
+SELECT @xml;
+
+EXEC XmlToTable @xml;
 
 GO
